fb6d4c6c6f2ce0c429e731c7f9dde8a55219c755
ToyotaPrius <114371673+ToyotaPrius9@users.noreply.github.com>
2022-12-17 20:39:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// Add your answer here
+$( ".lesson-icon").eq(0).hover(function(){
+    $( ".text-contents").eq(0).show();
+  }, function(){
+    $(".text-contents").hide();
+  })
+
+$( ".lesson-icon").eq(1).hover(function(){
+    $( ".text-contents").eq(1).show();
+  }, function(){
+    $(".text-contents").hide();
+  })
+
+$( ".lesson-icon").eq(2).hover(function(){
+    $( ".text-contents").eq(2).show();
+  }, function(){
+    $(".text-contents").hide();
+  })
+
+$( ".lesson-icon").eq(3).hover(function(){
+    $( ".text-contents").eq(3).show();
+  }, function(){
+    $(".text-contents").hide();
+  })
